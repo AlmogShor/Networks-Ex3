187cb98580a53d66cbd0f2abd4161f7faceb2af6
almogShor <shoralmog@gmail.com>
2021-11-25 13:32:19 +0200

    init

---
+++ Ex3.py
@@ -0,0 +1,36 @@
+# import socket module
+from socket import *
+import sys  # In order to terminate the program
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# Prepare a sever socket
+server_port = 80
+serverSocket.bind(('', server_port))
+serverSocket.listen(1)
+print("the server is ready receive")
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    print("connected from" + addr)
+    try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        # Send one HTTP header line into socket
+        serverSocket.send('https/1.1 200 OK\r\n\r\n'.encode())
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+
+        connectionSocket.close()
+    except IOError:
+        # Send response message for file not found
+        connectionSocket.send("404 Not Found".encode())
+        # Close client socket
+        connectionSocket.close()
+    serverSocket.close()
+    sys.exit()  # Terminate the program after sending the corresponding data
