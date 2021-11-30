48720acdbc05519edc50c012dfbe7a30f4e54317
almogShor <shoralmog@gmail.com>
2021-11-30 12:26:26 +0200

    Changes by Oriho

--- Ex3.py
+++ Ex3.py
@@ -1,10 +1,11 @@
 # import socket module
+import io
 from socket import *
 import sys  # In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 # Prepare a sever socket
-server_port = 8080
+server_port = 7777
 serverSocket.bind(('', server_port))
 serverSocket.listen(1)
 print("the server is ready receive")
@@ -13,7 +14,6 @@ while True:
     # Establish the connection
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
-    # print("connected from" + addr)
     try:
         message = connectionSocket.recv(2048).decode()
         filename = message.split()[1]
@@ -21,16 +21,17 @@ while True:
         outputdata = f.read()
         f.close()
         # Send one HTTP header line into socket
-        serverSocket.send('https/1.1 200 OK\r\n\r\n'.encode())
+        connectionSocket.send('\nHTTP/1.1 200 OK\n'.encode())
+        connectionSocket.send('Content-Type: text/html\n\n'.encode())
+
         # Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.send("\r\n".encode())
-
         connectionSocket.close()
     except IOError:
-        # Send response message for file not found
-        connectionSocket.send("404 Not Found".encode())
+        # Send response message for file not fou
+        connectionSocket.send('\nHTTP/1.1 404 not found\n'.encode())
         # Close client socket
         connectionSocket.close()
     serverSocket.close()
